e1c3860d9c24e136ce87670ca7e474e57bcb000b
Jeff M. Hubbard <jeffmhubbard@gmail.com>
2018-10-15 21:30:21 -0500

    Fix index error

--- LitStats.py
+++ LitStats.py
@@ -70,6 +70,9 @@ def get_stats(file):
     file = os.path.expanduser(file)
     with open(file) as fp:
         lines = fp.read().split('\n')
+        lc = len(lines)
+        if len(lines) < 42:
+            return None
         data = {
             'credits':          lines[0],
             'time_played':      lines[1],
@@ -121,149 +124,150 @@ def write_stats(scr,width):
     """Format and write out stats"""
     data = get_stats(__INFILE__)
 
-    credits = add_commas(data['credits'])
-    scr.addstr(1,16,credits,curses.color_pair(4))
+    if data is not None:
+        credits = add_commas(data['credits'])
+        scr.addstr(1,16,credits,curses.color_pair(4))
 
-    trade_rank, trade_title = get_rank_title(data['trade_title'])
-    trade_perc = data['trade_perc']
-    rank = cp_scale(trade_rank,30)
-    perc = cp_scale(trade_perc,100)
-    scr.addnstr(2,16,trade_rank,2,curses.color_pair(rank))
-    scr.addnstr(2,20,trade_perc+'%',4,curses.color_pair(perc))
-    scr.addnstr(2,25,trade_title,width-5)
+        trade_rank, trade_title = get_rank_title(data['trade_title'])
+        trade_perc = data['trade_perc']
+        rank = cp_scale(trade_rank,30)
+        perc = cp_scale(trade_perc,100)
+        scr.addnstr(2,16,trade_rank,2,curses.color_pair(rank))
+        scr.addnstr(2,20,trade_perc+'%',4,curses.color_pair(perc))
+        scr.addnstr(2,25,trade_title,width-5)
 
-    combat_rank, combat_title = get_rank_title(data['combat_title'])
-    combat_perc = data['combat_perc']
-    rank = cp_scale(combat_rank,30)
-    perc = cp_scale(combat_perc,100)
-    scr.addnstr(3,16,combat_rank,2,curses.color_pair(rank))
-    scr.addnstr(3,20,combat_perc+'%',4,curses.color_pair(perc))
-    scr.addnstr(3,25,combat_title,width-5)
+        combat_rank, combat_title = get_rank_title(data['combat_title'])
+        combat_perc = data['combat_perc']
+        rank = cp_scale(combat_rank,30)
+        perc = cp_scale(combat_perc,100)
+        scr.addnstr(3,16,combat_rank,2,curses.color_pair(rank))
+        scr.addnstr(3,20,combat_perc+'%',4,curses.color_pair(perc))
+        scr.addnstr(3,25,combat_title,width-5)
 
-    flc_perc = get_flc_perc(int(data['flc_rank']))
-    flc_perc = str(flc_perc) 
-    perc = cp_scale(flc_perc,100)
-    scr.addnstr(4,16,flc_perc+'%',width-21,curses.color_pair(perc))
+        flc_perc = get_flc_perc(int(data['flc_rank']))
+        flc_perc = str(flc_perc) 
+        perc = cp_scale(flc_perc,100)
+        scr.addnstr(4,16,flc_perc+'%',width-21,curses.color_pair(perc))
 
-    time_played = get_time_string(int(data['time_played']))
-    scr.addnstr(5,16,time_played,width-21)
+        time_played = get_time_string(int(data['time_played']))
+        scr.addnstr(5,16,time_played,width-21)
 
-    time_real = get_time_string(int(data['time_real']))
-    scr.addnstr(6,16,time_real,width-21)
+        time_real = get_time_string(int(data['time_real']))
+        scr.addnstr(6,16,time_real,width-21)
 
-    argon_rank, argon_title = get_rank_title(data['argon_title'])
-    argon_perc = data['argon_perc']
-    rank = cp_scale(argon_rank,10)
-    perc = cp_scale(argon_perc,100)
-    scr.addnstr(8,16,argon_rank,2,curses.color_pair(rank))
-    scr.addnstr(8,20,argon_perc+'%',4,curses.color_pair(perc))
-    scr.addnstr(8,25,argon_title,width-26)
+        argon_rank, argon_title = get_rank_title(data['argon_title'])
+        argon_perc = data['argon_perc']
+        rank = cp_scale(argon_rank,10)
+        perc = cp_scale(argon_perc,100)
+        scr.addnstr(8,16,argon_rank,2,curses.color_pair(rank))
+        scr.addnstr(8,20,argon_perc+'%',4,curses.color_pair(perc))
+        scr.addnstr(8,25,argon_title,width-26)
 
-    boron_rank, boron_title = get_rank_title(data['boron_title'])
-    boron_perc = data['boron_perc']
-    rank = cp_scale(boron_rank,10)
-    perc = cp_scale(boron_perc,100)
-    scr.addnstr(9,16,boron_rank,2,curses.color_pair(rank))
-    scr.addnstr(9,20,boron_perc+'%',4,curses.color_pair(perc))
-    scr.addnstr(9,25,boron_title,width-26)
+        boron_rank, boron_title = get_rank_title(data['boron_title'])
+        boron_perc = data['boron_perc']
+        rank = cp_scale(boron_rank,10)
+        perc = cp_scale(boron_perc,100)
+        scr.addnstr(9,16,boron_rank,2,curses.color_pair(rank))
+        scr.addnstr(9,20,boron_perc+'%',4,curses.color_pair(perc))
+        scr.addnstr(9,25,boron_title,width-26)
 
-    paranid_rank, paranid_title = get_rank_title(data['paranid_title'])
-    paranid_perc = data['paranid_perc']
-    rank = cp_scale(paranid_rank,10)
-    perc = cp_scale(paranid_perc,100)
-    scr.addnstr(10,16,paranid_rank,2,curses.color_pair(rank))
-    scr.addnstr(10,20,paranid_perc+'%',4,curses.color_pair(perc))
-    scr.addnstr(10,25,paranid_title,width-26)
+        paranid_rank, paranid_title = get_rank_title(data['paranid_title'])
+        paranid_perc = data['paranid_perc']
+        rank = cp_scale(paranid_rank,10)
+        perc = cp_scale(paranid_perc,100)
+        scr.addnstr(10,16,paranid_rank,2,curses.color_pair(rank))
+        scr.addnstr(10,20,paranid_perc+'%',4,curses.color_pair(perc))
+        scr.addnstr(10,25,paranid_title,width-26)
 
-    split_rank, split_title = get_rank_title(data['split_title'])
-    split_perc = data['split_perc']
-    rank = cp_scale(split_rank,10)
-    perc = cp_scale(split_perc,100)
-    scr.addnstr(11,16,split_rank,2,curses.color_pair(rank))
-    scr.addnstr(11,20,split_perc+'%',4,curses.color_pair(perc))
-    scr.addnstr(11,25,split_title,width-26)
+        split_rank, split_title = get_rank_title(data['split_title'])
+        split_perc = data['split_perc']
+        rank = cp_scale(split_rank,10)
+        perc = cp_scale(split_perc,100)
+        scr.addnstr(11,16,split_rank,2,curses.color_pair(rank))
+        scr.addnstr(11,20,split_perc+'%',4,curses.color_pair(perc))
+        scr.addnstr(11,25,split_title,width-26)
 
-    teladi_rank, teladi_title = get_rank_title(data['teladi_title'])
-    teladi_perc = data['teladi_perc']
-    rank = cp_scale(teladi_rank,10)
-    perc = cp_scale(teladi_perc,100)
-    scr.addnstr(12,16,teladi_rank,2,curses.color_pair(rank))
-    scr.addnstr(12,20,teladi_perc+'%',4,curses.color_pair(perc))
-    scr.addnstr(12,25,teladi_title,width-26)
+        teladi_rank, teladi_title = get_rank_title(data['teladi_title'])
+        teladi_perc = data['teladi_perc']
+        rank = cp_scale(teladi_rank,10)
+        perc = cp_scale(teladi_perc,100)
+        scr.addnstr(12,16,teladi_rank,2,curses.color_pair(rank))
+        scr.addnstr(12,20,teladi_perc+'%',4,curses.color_pair(perc))
+        scr.addnstr(12,25,teladi_title,width-26)
 
-    terran_rank, terran_title = get_rank_title(data['terran_title'])
-    terran_perc = data['terran_perc']
-    rank = cp_scale(terran_rank,10)
-    perc = cp_scale(terran_perc,100)
-    scr.addnstr(13,16,terran_rank,2,curses.color_pair(rank))
-    scr.addnstr(13,20,terran_perc+'%',4,curses.color_pair(perc))
-    scr.addnstr(13,25,terran_title,width-26)
+        terran_rank, terran_title = get_rank_title(data['terran_title'])
+        terran_perc = data['terran_perc']
+        rank = cp_scale(terran_rank,10)
+        perc = cp_scale(terran_perc,100)
+        scr.addnstr(13,16,terran_rank,2,curses.color_pair(rank))
+        scr.addnstr(13,20,terran_perc+'%',4,curses.color_pair(perc))
+        scr.addnstr(13,25,terran_title,width-26)
 
-    atf_rank, atf_title = get_rank_title(data['atf_title'])
-    atf_perc = data['atf_perc']
-    rank = cp_scale(atf_rank,10)
-    perc = cp_scale(atf_perc,100)
-    scr.addnstr(14,16,atf_rank,2,curses.color_pair(rank))
-    scr.addnstr(14,20,atf_perc+'%',4,curses.color_pair(perc))
-    scr.addnstr(14,25,atf_title,width-26)
+        atf_rank, atf_title = get_rank_title(data['atf_title'])
+        atf_perc = data['atf_perc']
+        rank = cp_scale(atf_rank,10)
+        perc = cp_scale(atf_perc,100)
+        scr.addnstr(14,16,atf_rank,2,curses.color_pair(rank))
+        scr.addnstr(14,20,atf_perc+'%',4,curses.color_pair(perc))
+        scr.addnstr(14,25,atf_title,width-26)
 
-    goner_rank, goner_title = get_rank_title(data['goner_title'])
-    goner_perc = data['goner_perc']
-    rank = cp_scale(goner_rank,10)
-    perc = cp_scale(goner_perc,100)
-    scr.addnstr(15,16,goner_rank,2,curses.color_pair(rank))
-    scr.addnstr(15,20,goner_perc+'%',4,curses.color_pair(perc))
-    scr.addnstr(15,25,goner_title,width-26)
+        goner_rank, goner_title = get_rank_title(data['goner_title'])
+        goner_perc = data['goner_perc']
+        rank = cp_scale(goner_rank,10)
+        perc = cp_scale(goner_perc,100)
+        scr.addnstr(15,16,goner_rank,2,curses.color_pair(rank))
+        scr.addnstr(15,20,goner_perc+'%',4,curses.color_pair(perc))
+        scr.addnstr(15,25,goner_title,width-26)
 
-    yaki_rank, yaki_title = get_rank_title(data['yaki_title'])
-    yaki_perc = data['yaki_perc']
-    rank = cp_scale(yaki_rank,10)
-    perc = cp_scale(yaki_perc,100)
-    scr.addnstr(16,16,yaki_rank,2,curses.color_pair(rank))
-    scr.addnstr(16,20,yaki_perc+'%',4,curses.color_pair(perc))
-    scr.addnstr(16,25,yaki_title,width-26)
+        yaki_rank, yaki_title = get_rank_title(data['yaki_title'])
+        yaki_perc = data['yaki_perc']
+        rank = cp_scale(yaki_rank,10)
+        perc = cp_scale(yaki_perc,100)
+        scr.addnstr(16,16,yaki_rank,2,curses.color_pair(rank))
+        scr.addnstr(16,20,yaki_perc+'%',4,curses.color_pair(perc))
+        scr.addnstr(16,25,yaki_title,width-26)
 
-    pirates_rank, pirates_title = get_rank_title(data['pirates_title'])
-    pirates_perc = data['pirates_perc']
-    rank = cp_scale(pirates_rank,10)
-    perc = cp_scale(pirates_perc,100)
-    scr.addnstr(17,16,pirates_rank,2,curses.color_pair(rank))
-    scr.addnstr(17,20,pirates_perc+'%',4,curses.color_pair(perc))
-    scr.addnstr(17,25,pirates_title,width-26)
+        pirates_rank, pirates_title = get_rank_title(data['pirates_title'])
+        pirates_perc = data['pirates_perc']
+        rank = cp_scale(pirates_rank,10)
+        perc = cp_scale(pirates_perc,100)
+        scr.addnstr(17,16,pirates_rank,2,curses.color_pair(rank))
+        scr.addnstr(17,20,pirates_perc+'%',4,curses.color_pair(perc))
+        scr.addnstr(17,25,pirates_title,width-26)
 
-    count_m5 = add_commas(data['count_m5'])
-    scr.addnstr(19,6,count_m5,6)
-    count_m4 = add_commas(data['count_m4'])
-    scr.addnstr(20,6,count_m4,6)
-    count_m3 = add_commas(data['count_m3'])
-    scr.addnstr(21,6,count_m3,6)
-    count_m6 = add_commas(data['count_m6'])
-    scr.addnstr(22,6,count_m6,6)
+        count_m5 = add_commas(data['count_m5'])
+        scr.addnstr(19,6,count_m5,6)
+        count_m4 = add_commas(data['count_m4'])
+        scr.addnstr(20,6,count_m4,6)
+        count_m3 = add_commas(data['count_m3'])
+        scr.addnstr(21,6,count_m3,6)
+        count_m6 = add_commas(data['count_m6'])
+        scr.addnstr(22,6,count_m6,6)
 
-    count_m8 = add_commas(data['count_m8'])
-    scr.addnstr(19,18,count_m8,6)
-    count_m7 = add_commas(data['count_m7'])
-    scr.addnstr(20,18,count_m7,6)
-    count_m1 = add_commas(data['count_m1'])
-    scr.addnstr(21,18,count_m1,6)
-    count_m2 = add_commas(data['count_m2'])
-    scr.addnstr(22,18,count_m2,6)
+        count_m8 = add_commas(data['count_m8'])
+        scr.addnstr(19,18,count_m8,6)
+        count_m7 = add_commas(data['count_m7'])
+        scr.addnstr(20,18,count_m7,6)
+        count_m1 = add_commas(data['count_m1'])
+        scr.addnstr(21,18,count_m1,6)
+        count_m2 = add_commas(data['count_m2'])
+        scr.addnstr(22,18,count_m2,6)
 
-    count_tm = add_commas(data['count_tm'])
-    scr.addnstr(19,30,count_tm,6)
-    count_ts = add_commas(data['count_ts'])
-    scr.addnstr(20,30,count_ts,6)
-    count_tp = add_commas(data['count_tp'])
-    scr.addnstr(21,30,count_tp,6)
-    count_tl = add_commas(data['count_tl'])
-    scr.addnstr(22,30,count_tl,6)
+        count_tm = add_commas(data['count_tm'])
+        scr.addnstr(19,30,count_tm,6)
+        count_ts = add_commas(data['count_ts'])
+        scr.addnstr(20,30,count_ts,6)
+        count_tp = add_commas(data['count_tp'])
+        scr.addnstr(21,30,count_tp,6)
+        count_tl = add_commas(data['count_tl'])
+        scr.addnstr(22,30,count_tl,6)
 
-    count_station = add_commas(data['count_station'])
-    scr.addnstr(19,42,count_station,6)
-    count_sat = add_commas(data['count_sat'])
-    scr.addnstr(20,42,count_sat,6)
+        count_station = add_commas(data['count_station'])
+        scr.addnstr(19,42,count_station,6)
+        count_sat = add_commas(data['count_sat'])
+        scr.addnstr(20,42,count_sat,6)
 
-    scr.refresh()
+        scr.refresh()
 
 
 def window(scr,y,x):
